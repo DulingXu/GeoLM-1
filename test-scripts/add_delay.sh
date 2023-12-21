d854765a05534a8b94fe0b4b592c1023c4e0b2f8
dfzhang2022 <dfzhang@ruc.edu.cn>
2023-12-21 10:17:31 +0800

    zdf | update scripts.

--- test-scripts/add_delay.sh
+++ test-scripts/add_delay.sh
@@ -4,15 +4,36 @@
 CONTAINERS=(glee1 glee2 glee3)
 
 # 各容器对应的时延
-DELAYS=(50ms 30ms 20ms)
+DELAYS=(70ms 60ms 10ms)
 
-for i in "${!CONTAINERS[@]}"
-do
-    # 获取容器的eth0接口
-     eth0=$(docker inspect ${CONTAINERS[$i]} | jq -r '.[0].NetworkSettings.Networks.brige.EndpointID')
-  #  eth0=$(docker inspect ${CONTAINERS[$i]} | jq -r '.[0].NetworkSettings.Networks.glee-net.EndpointID')
-    eth0=$(docker inspect ${CONTAINERS[$i]} |grep -o '"EndpointID": "[^"]*"' | cut -d'"' -f4)
- #   echo ${eth0}
-    # 在该接口上添加时延
-    tc qdisc add dev ${eth0} root netem delay ${DELAYS[$i]}
+# for i in "${!CONTAINERS[@]}"
+# do
+#     # 获取容器的eth0接口
+#    #  eth0=$(docker inspect ${CONTAINERS[$i]} | jq -r '.[0].NetworkSettings.Networks.bridge.EndpointID')
+#    #  echo ${eth0}
+#    #  eth0=$(docker inspect ${CONTAINERS[$i]} |grep -o '"EndpointID": "[^"]*"' | cut -d'"' -f4)
+#    #  echo ${eth0}
+
+
+#     # 在该接口上添加时延
+#    #  tc qdisc add dev ${eth0} root netem delay ${DELAYS[$i]}
+# done
+
+# 遍历数组，为每个容器添加延迟
+for ((i=0; i<${#CONTAINERS[@]}; i++)); do
+    CONTAINER=${CONTAINERS[i]}
+    DELAY=${DELAYS[i]}
+    echo ${CONTAINER}
+    echo ${DELAY}
+
+    #删除存在的时延
+    docker exec -it ${CONTAINER} tc qdisc del dev eth0 root
+
+   #  docker exec -it  ${CONTAINER} apt-get update
+   #  docker exec -it  ${CONTAINER} apt-get install -y iproute2
+
+    # 使用 docker exec 在容器中执行 tc 命令
+    docker exec -it ${CONTAINER} tc qdisc add dev eth0 root netem delay ${DELAY}
 done
+
+# docker exec -it glee1 tc qdisc add dev eth0 root netem delay 20ms
--- test-scripts/stop_and_clean_etcd.sh
+++ test-scripts/stop_and_clean_etcd.sh
@@ -7,27 +7,30 @@ CONTAINERS=("glee1" "glee2" "glee3")
 # 停止容器中的glee-etcd进程
 for container in ${CONTAINERS[@]} 
 do
-  docker exec $container pkill glee-etcd
+  docker exec ${container} pkill glee-etcd
 done 
 
-# 解除端口绑定
-for container in ${CONTAINERS[@]}
-do
-  # 检查2380端口
-  PID=$(docker exec $container lsof -ti :2380)
-  if [ ! -z "$PID" ]; then
-    docker exec $container kill $PID
-  fi
+# # 解除端口绑定
+# for container in ${CONTAINERS[@]}
+# do
+#   # 检查2380端口
+#   PID=$(docker exec $container lsof -ti :2380)
+#   if [ ! -z "$PID" ]; then
+#     docker exec ${container} kill ${PID}
+#   fi
   
-  # 检查2379端口
-  PID=$(docker exec $container lsof -ti :2379)
-  if [ ! -z "$PID" ]; then  
-    docker exec $container kill $PID
-  fi
-done
+#   # 检查2379端口
+#   PID=$(docker exec $container lsof -ti :2379)
+#   if [ ! -z "$PID" ]; then  
+#     docker exec ${container} kill ${PID}
+#   fi
+# done
 
 # 清空etcd数据目录
 for container in ${CONTAINERS[@]}
-do 
-  docker exec $container rm -rf /expr/data/*
+do
+  # docker exec ${container} kill -9 `pidof glee-etcd`
+  echo ${container}
+  docker exec ${container} rm -rf /expr/data/
 done
+
